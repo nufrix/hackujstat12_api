1b180da9692e302e894662059ebd515f210d51cf
Dan Pressl <Dan.Pressl@lmc.eu>
2018-09-14 20:21:24 +0200

    Added new endpoint for candidate detail /candidate/<int:candidate_id>, app now runs on 0.0.0.0

--- app.py
+++ app.py
@@ -8,12 +8,10 @@ from flask import Flask, g, jsonify, request
 import sqlite3
 
 HLIDACSTATU_API_TOKEN = '7f7297e6fe4842ffac1820e250a909d0'
-
+DATABASE = 'data/api_data.db'
 
 app = Flask(__name__)
 
-
-DATABASE = 'data/api_data.db'
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -42,7 +40,6 @@ def find_candidates():
 
         /find-candidates?query=Jiří Nová
     """
-    print(request.args)
     query = request.args.get('query').encode('utf-8')
     if query is None:
         raise ValueError('Missing request argument \"query\"')
@@ -52,15 +49,28 @@ def find_candidates():
         cursor.execute('SELECT * FROM candidate WHERE FULLNAME LIKE "%{}%"'.format(query))
         result = cursor.fetchall()
 
-        return jsonify(candidates=[{'id': item[-1],'fullname': item[-2], 'age': item[10], 'work': item[11], 'home': item[12]} for item in result])
+        return jsonify(candidates=[{'id': item[-1], 'fullname': item[-2], 'age': item[10]} for item in result])
+
+@app.route('/candidate/<int:candidate_id>', methods=['GET'])
+def candidate(candidate_id):
+    """Get details for given candidate."""
+    with app.app_context():
+        cursor = get_db().cursor()
+        cursor.execute('SELECT * FROM candidate WHERE ID={}'.format(candidate_id))
+        result = cursor.fetchone()
+
+        return jsonify(fullname=result[-2], age=result[10], work=result[11], home=result[12], region=result[0],
+                       party=result[4], council=result[1], vote_region=result[2])
+
 
 @app.errorhandler(Exception)
 def handle_error(e):
     code = 500
     if isinstance(e, HTTPException):
         code = e.code
+    app.logger.exception('Unhandled exception.')
     return jsonify(error=str(e)), code
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
